39994abe8649314c327a04bbca26dbbadc42166b
Larwive <larwive@gmail.com>
2026-01-22 22:27:56 +0100

    Add Ke computation for heat equation

---
+++ include/thermal_fea/physics/heat_equation.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <array>
+
+namespace thermal_fea::physics {
+
+typedef std::array<std::array<double, 3>, 3> Matrix3x3;
+
+Matrix3x3 get_Ke(const thermal_fea::mesh::Mesh &mesh,
+                 const thermal_fea::mesh::Element &element, const double k);
+
+} // namespace thermal_fea::physics
