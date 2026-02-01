deab8820d0b7156aa0b9c00abd464f7fc87de700
SaDaKa <kanitbensaidi.ac@gmail.com>
2026-02-01 12:22:00 +0100

    Cleanup for merge

--- src/solver/conjugate_gradient.cpp
+++ src/solver/conjugate_gradient.cpp
@@ -11,35 +11,6 @@ namespace thermal_fea::solver {
 thermal_fea::linalg::Vector
 conjugate_gradient(const thermal_fea::linalg::Matrix &A,
                    const thermal_fea::linalg::Vector &b) {
-  if (b.norm() < 1e-12) {
-    return thermal_fea::linalg::Vector(b.size()); // vecteur nul
-  }
-  thermal_fea::linalg::Vector x(b.size());
-  thermal_fea::linalg::Vector r = b - A * x;
-  thermal_fea::linalg::Vector r_new(r.size());
-  thermal_fea::linalg::Vector p = r;
-  double alpha = 0.0;
-  double beta = 0.0;
-
-  for (int i = 0; i < 100; ++i) {
-    alpha = r.dot(r) / p.dot(A * p);
-    x += alpha * p;
-    r_new = r - alpha * A * p;
-    if (r_new.norm() < 1e-6) {
-      break;
-    }
-    beta = r_new.dot(r_new) / r.dot(r);
-    p = r_new + beta * p;
-    r = r_new;
-  }
-
-  return x;
-}
-
-thermal_fea::linalg::Vector
-conjugate_gradient_optimized(const thermal_fea::linalg::Matrix &A,
-                   const thermal_fea::linalg::Vector &b) {
-  //return conjugate_gradient(A,b);
 
   if (b.norm() < 1e-12) {
     return thermal_fea::linalg::Vector(b.size()); // vecteur nul
@@ -65,7 +36,6 @@ conjugate_gradient_optimized(const thermal_fea::linalg::Matrix &A,
     p = r_new + beta * p;
     r = r_new;
   }
-  std::cout << "CG iterations: " << i + 1 << std::endl;
   return x;
 }
 
